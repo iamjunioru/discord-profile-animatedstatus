f1969e14d5116b605ffebb6b9baed36d538036dc
Junior <iamjunioru@gmail.com>
2024-03-31 16:12:08 -0300

    feat: install and start .bat

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+title Instalador
+echo Instalando..
+py -m pip install -r requirements.txt
+echo O necessário foi instalado.
+echo Agora inicie o arquivo start.bat para dar um oi para o bot!
+pause
